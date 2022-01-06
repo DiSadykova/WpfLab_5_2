868bb89cada5652c5f5235829676125789a9aac3
DiSadykova <dilyara.mozhet.sadykova@gmail.com>
2022-01-06 17:31:07 +0300

    Добавлены обработчики событий на кнопки цветов, кроме чб

--- WpfLab_5_2_2/MainWindow.xaml.cs
+++ WpfLab_5_2_2/MainWindow.xaml.cs
@@ -28,13 +28,68 @@ namespace WpfLab_5_2_2
             InitializeComponent();
         }
 
-       // private void MenuItem_Click_1(object sender, RoutedEventArgs e)
-       // {
-       //     SaveFileDialog saveFileDialog = new SaveFileDialog();
-       //     if (saveFileDialog.ShowDialog() == true)
-       //     {
-       //        File.(saveFileDialog.FileName, inkCanvas.Strokes);
-       //     }
-       // }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color=Colors.Red;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Orange;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Yellow;
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Magenta;
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+
+        //private void Button_Click_10(object sender, RoutedEventArgs e)
+        //{
+        //    inkCanvas.DefaultDrawingAttributes.Color = ;
+        //}
+
+        //private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        // {
+        //     SaveFileDialog saveFileDialog = new SaveFileDialog();
+        //     if (saveFileDialog.ShowDialog() == true)
+        //     {
+        //        File. (saveFileDialog.FileName, inkCanvas.Strokes);
+        //     }
+        // }
     }
 }
